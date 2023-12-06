397b3450dbf477aa4bde85e294f0d80232739831
ahmed uzair <ahmeduzair.nu@gmail.com>
2023-12-06 23:16:53 +0500

    structure refactored

---
+++ ShoppingCart/ShoppingCart.API/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopingCart.Core.Entities;
+using ShopingCart.Core.Interface;
+using ShoppingCart.Core.Attribtues;
+
+namespace ShoppingCart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Auth]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetCartItems()
+        {
+            try
+            {
+                var items = await _cartRepository.GetAllCartItemsAsync();
+                return Ok(items);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCartItems(int id)
+        {
+            try
+            {
+                var product = await _cartRepository.GetCartItemByIdAsync(id);
+                if (product == null) return NotFound();
+
+                return Ok(product);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckOut(CartItem item)
+        {
+            try
+            {
+                await _cartRepository.AddCartItemAsync(item);
+                return CreatedAtAction(nameof(GetCartItems), new { id = item.Id }, item);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.API/Controllers/CheckoutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopingCart.Core.Entities;
+using ShopingCart.Core.Interface;
+using ShoppingCart.Core.Attribtues;
+
+namespace ShoppingCart.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Auth]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly ICheckoutRepository _checkoutRepository;
+
+        public CheckoutController(ICheckoutRepository checkoutRepository)
+        {
+            _checkoutRepository = checkoutRepository;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CheckOut(List<CartItem> cartItems)
+        {
+            try
+            {
+                await _checkoutRepository.CheckOutCartAsync(cartItems);
+                return Ok(nameof(CheckOut));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
